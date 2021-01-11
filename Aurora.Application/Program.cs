8362d1124d663ba60f3f881eb561b28d368a1eaf
neicostajorge <neicostajorge@gmail.com>
2021-01-11 20:54:29 -0300

    subindo o codigo

---
+++ Aurora.Application/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Aurora.Application
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+                .ConfigureAppConfiguration((ctx, config) =>
+                {
+                    var env = ctx.HostingEnvironment;
+                    config
+                        .SetBasePath(AppContext.BaseDirectory)
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                        .AddEnvironmentVariables();
+                })
+                .ConfigureLogging(logging => logging.AddConsole());
+    }
+    }
+}
